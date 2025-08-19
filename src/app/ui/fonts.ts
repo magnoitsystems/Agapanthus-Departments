1b3a7355eef94f334963eb562c5e68403b91da40
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-18 23:35:49 -0300

    finish setting up all. everything ready to startttttt :)

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Abyssinica_SIL} from 'next/font/google'
+
+export const abyssinica = Abyssinica_SIL({weight: '400', subsets: ['latin']})
+
+/* Para usar en algún lado
+* import {abyssinica} from "@/app/ui/fonts";
+* `${abyssinica.className}`
+*/
