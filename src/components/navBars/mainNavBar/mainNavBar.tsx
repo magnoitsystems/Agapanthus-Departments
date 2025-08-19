dd3f4c3045339c4dbd95342556c8e52644b1aaf8
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-19 11:52:42 -0300

    finish implementing welcome texts

--- src/components/navBars/mainNavBar/mainNavBar.tsx
+++ src/components/navBars/mainNavBar/mainNavBar.tsx
@@ -1,5 +1,6 @@
 import styles from './mainNavBar.module.css';
 import Image from 'next/image';
+import SectionsBar from "@/components/navBars/mainNavBar/sectionsBar";
 
 export default function MainNavBar() {
     return (
@@ -9,36 +10,11 @@ export default function MainNavBar() {
             src={"/logos/blanco.png"}
             alt={"logo rectangular de Agapanthus - Cabañas Las Marias"}
             width={444}
-            height={90}
+            height={85}
           />
         </nav>
 
-        <div className={styles.sectionsBar}>
-          <div className={`${styles.section} ${styles.classic}`}>
-            <h3>Nuestro complejo</h3>
-          </div>
-
-          <div className={`${styles.section} ${styles.dropdown}`}>
-            <div className={styles.mainInfo}>
-              <h3>Cabañas</h3>
-                {/*<Image
-                src={"/icons/downSemiArrow.png"}
-                alt={"semi flecha apuntando hacia abajo"}
-                width={24}
-                height={24}
-              />*/}
-            </div>
-
-            <div className={styles.deptos}>
-              <h3>AQUA - 3 personas</h3>
-              <h3>LAVANDA - 5 personas</h3>
-            </div>
-          </div>
-
-          <div className={`${styles.section} ${styles.classic}`}>
-            <h3>Reservas</h3>
-          </div>
-        </div>
+        <SectionsBar/>
       </main>
     );
 }
