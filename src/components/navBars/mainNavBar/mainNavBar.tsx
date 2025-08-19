2b13e588df6448fea995ac7451ee320f33e1da44
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-19 10:55:43 -0300

    started implementing main nav bar

--- src/components/navBars/mainNavBar/mainNavBar.tsx
+++ src/components/navBars/mainNavBar/mainNavBar.tsx
@@ -6,12 +6,26 @@ export default function MainNavBar() {
         <main className={styles.main}>
             <nav className={styles.nav}>
                 <Image
-                    src={'/logos/rectangular.png'}
+                    src={'/logos/blanco.png'}
                     alt={'logo rectangular de Agapanthus - Cabañas Las Marias'}
                     width={444}
                     height={90}
                 />
             </nav>
+
+            <div className={styles.sectionsBar}>
+                <div className={`${styles.section} ${styles.classic}`}>
+                    <h4>Nuestro complejo</h4>
+                </div>
+
+                <div className={`${styles.section} ${styles.dropdown}`}>
+                    <h4>Cabañas</h4>
+                </div>
+
+                <div className={`${styles.section} ${styles.classic}`}>
+                    <h4>Reservas</h4>
+                </div>
+            </div>
         </main>
     )
 }
