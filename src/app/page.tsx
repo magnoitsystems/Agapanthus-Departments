36e61327ff5fcba49296cd9fe89b8d567ac07a6c
Ana Bellinzona <anabellinzona@gmail.com>
2025-08-19 13:56:54 -0300

    adjunto formulario para reservar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import styles from "./page.module.css";
 import MainNavBar from "@/components/navBars/mainNavBar/mainNavBar";
 import Welcome from "@/components/welcome/welcome";
 import Footer from "@/components/footer/footer"
+import Form from "./(views)/form/page";
 
 export default function Home() {
   return (
     <div className={styles.page}>
         <MainNavBar/>
         <Welcome/>
+        <Form />
         <Footer />
     </div>
   );
