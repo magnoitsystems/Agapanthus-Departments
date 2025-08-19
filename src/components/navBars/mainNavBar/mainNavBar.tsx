bb8601ae99d4a98122aa90735ef215f2a2ed2365
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-19 11:37:29 -0300

    finish implementing main nav bar

--- src/components/navBars/mainNavBar/mainNavBar.tsx
+++ src/components/navBars/mainNavBar/mainNavBar.tsx
@@ -3,29 +3,42 @@ import Image from 'next/image';
 
 export default function MainNavBar() {
     return (
-        <main className={styles.main}>
-            <nav className={styles.nav}>
-                <Image
-                    src={'/logos/blanco.png'}
-                    alt={'logo rectangular de Agapanthus - Cabañas Las Marias'}
-                    width={444}
-                    height={90}
-                />
-            </nav>
+      <main className={styles.main}>
+        <nav className={styles.nav}>
+          <Image
+            src={"/logos/blanco.png"}
+            alt={"logo rectangular de Agapanthus - Cabañas Las Marias"}
+            width={444}
+            height={90}
+          />
+        </nav>
 
-            <div className={styles.sectionsBar}>
-                <div className={`${styles.section} ${styles.classic}`}>
-                    <h3>Nuestro complejo</h3>
-                </div>
+        <div className={styles.sectionsBar}>
+          <div className={`${styles.section} ${styles.classic}`}>
+            <h3>Nuestro complejo</h3>
+          </div>
 
-                <div className={`${styles.section} ${styles.dropdown}`}>
-                    <h3>Cabañas</h3>
-                </div>
-
-                <div className={`${styles.section} ${styles.classic}`}>
-                    <h3>Reservas</h3>
-                </div>
+          <div className={`${styles.section} ${styles.dropdown}`}>
+            <div className={styles.mainInfo}>
+              <h3>Cabañas</h3>
+                {/*<Image
+                src={"/icons/downSemiArrow.png"}
+                alt={"semi flecha apuntando hacia abajo"}
+                width={24}
+                height={24}
+              />*/}
             </div>
-        </main>
-    )
+
+            <div className={styles.deptos}>
+              <h3>AQUA - 3 personas</h3>
+              <h3>LAVANDA - 5 personas</h3>
+            </div>
+          </div>
+
+          <div className={`${styles.section} ${styles.classic}`}>
+            <h3>Reservas</h3>
+          </div>
+        </div>
+      </main>
+    );
 }
