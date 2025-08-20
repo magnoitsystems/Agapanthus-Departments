af2f9313e114ea4c27226d37a3045c4321cf5fea
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-19 21:43:34 -0300

    added redirections to secondary nav bar

--- src/components/navBars/secondNavBar/secondNavBar.tsx
+++ src/components/navBars/secondNavBar/secondNavBar.tsx
@@ -1,16 +1,19 @@
+'use client'
+
 import styles from "./secondNavBar.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function secondNavBar(){
     return(
         <nav className={styles.navBarProperties}>
             <div>
-                <Image src={"/logos/logoComplejoNavBar.png"} alt={"Logo complejo"} width={400} height={80}/>
+                <Image src={"/logos/white.png"} alt={"Logo complejo"} width={418} height={100}/>
             </div>
             <div className={styles.navSectionsProperties}>
-                <a href={"/"}>Nuestro complejo</a>
-                <a href={"/cabanias"}>Cabañas</a>
-                <a href={"/form"}>Reservas</a>
+                <Link href="/#nuestro-complejo"><h4>Nuestro complejo</h4></Link>
+                <Link href="/#cabanas"><h4>Cabañas</h4></Link>
+                <Link href="/#reservas"><h4>Reservas</h4></Link>
             </div>
         </nav>
     );
