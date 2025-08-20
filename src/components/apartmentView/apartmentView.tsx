f7c22abded2f239e95e74b6ff5e913a1495194ad
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-20 19:55:13 -0300

    add mobile styles for the details component

--- src/components/apartmentView/apartmentView.tsx
+++ src/components/apartmentView/apartmentView.tsx
@@ -10,35 +10,30 @@ import ToggleButton from '../seeMoreButton/toggleButton';
 import { aqua, lavanda, aquaImages, lavandaImages } from "../apartmentDetails/apartmentsData";
 
 const ApartmentView = () => {
-    const [isDetailsView, setIsDetailsView] = useState(true);
-    const searchParams = useSearchParams();
-    const id = searchParams.get("id");
+  const [isDetailsView, setIsDetailsView] = useState(true);
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
 
-    const details = id === "2" ? lavanda : aqua;
-    const images = id === "2" ? lavandaImages : aquaImages;
-    const title = id === "2" ? "Cabaña Lavanda" : "Cabaña Aqua";
+  const details = id === "2" ? lavanda : aqua;
+  const images  = id === "2" ? lavandaImages : aquaImages;
+  const title   = id === "2" ? "Cabaña Lavanda" : "Cabaña Aqua";
 
-    const toggleView = () => setIsDetailsView(!isDetailsView);
+  const toggleView = () => setIsDetailsView(!isDetailsView);
 
-    return (
-        <div className={styles.mainContainer}>
-            // ApartmentView.tsx
+  return (
+    <div className={styles.mainContainer}>
+      {/* ApartmentView */}
+      <Carousel images={images} showControls={!isDetailsView} dimmed={isDetailsView} />
 
-            <Carousel images={images} showControls={!isDetailsView} dimmed={isDetailsView} />
+      <div className={styles.contentOverlay}>
+        {isDetailsView && <ApartmentDetails title={title} details={details} />}
+      </div>
 
-
-            <div className={styles.contentOverlay}>
-                {isDetailsView && <ApartmentDetails title={title} details={details} />}
-            </div>
-
-            <div className={styles.buttonContainer}>
-                <ToggleButton
-                    isDetailsView={isDetailsView}
-                    onClick={toggleView}
-                />
-            </div>
-        </div>
-    );
+      <div className={styles.buttonContainer}>
+        <ToggleButton isDetailsView={isDetailsView} onClick={toggleView} />
+      </div>
+    </div>
+  );
 };
 
 export default ApartmentView;
