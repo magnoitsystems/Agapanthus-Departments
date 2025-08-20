fa23bbbd8487f12c959d75501c2c78d8caae2db2
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-20 20:13:34 -0300

    solve error in vercel adding suspense tag

--- src/app/(views)/detalles/page.tsx
+++ src/app/(views)/detalles/page.tsx
@@ -3,16 +3,15 @@ import ToggleButton from "@/components/seeMoreButton/toggleButton";
 import styles from "./page.module.css";
 import Carousel from "@/components/carrousel/carousel";
 import ApartmentView from "@/components/apartmentView/apartmentView";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <main className={styles.main}>
       <SecondNavBar />
-      {/* <Carousel />
-      <div className={styles.toggleButtonContainer}>
-        <ToggleButton />
-      </div> */}
-      <ApartmentView />
+       <Suspense fallback={<div>Cargando...</div>}>
+        <ApartmentView />
+      </Suspense>
     </main>
   );
 }
