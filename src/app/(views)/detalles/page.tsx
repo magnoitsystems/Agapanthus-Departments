26ec23f149c4b588f4c637de8cb704c2a18f8dce
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-08-20 20:59:27 -0300

    change styles in gallery

--- src/app/(views)/detalles/page.tsx
+++ src/app/(views)/detalles/page.tsx
@@ -1,7 +1,5 @@
 import SecondNavBar from "@/components/navBars/secondNavBar/secondNavBar";
-import ToggleButton from "@/components/seeMoreButton/toggleButton";
 import styles from "./page.module.css";
-import Carousel from "@/components/carrousel/carousel";
 import ApartmentView from "@/components/apartmentView/apartmentView";
 import { Suspense } from "react";
 
