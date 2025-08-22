f7d70e8d3cc88af1810ba8f4f45dd6245dccbd72
Agostina Bidegain <agostinabidegain@gmail.com>
2025-08-22 20:52:30 -0300

    some changes at review

--- src/components/reviews/reviewsSection.tsx
+++ src/components/reviews/reviewsSection.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import styles from "./reviewsSection.module.css";
 import { abyssinica } from "@/app/ui/fonts";
 import { useOpiniones } from "@/hooks/useOpinions";
-import SuccessAlert from "@/components/successAlert/successAlert";
+import { motion } from "framer-motion";
 
 export default function ReviewsSection() {
   const [name, setName] = useState("");
@@ -14,6 +14,8 @@ export default function ReviewsSection() {
   const [showAlert, setShowAlert] = useState(false);
   const [buttonText, setButtonText] = useState("Enviar reseña");
   const [buttonActive, setButtonActive] = useState(false);
+
+  const [paused, setPaused] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -41,84 +43,66 @@ export default function ReviewsSection() {
     }
   };
 
-  const baseDuration = 17;
-  const animationDuration = opiniones.length > 0 ? baseDuration * (opiniones.length / 3) : baseDuration;
-
   return (
-      <section className={styles.reviewsSection}>
-        <div className={styles.container}>
-          <div className={styles.header}>
-            <h1 className={`${styles.title} ${abyssinica.className}`}>
-              Si nos conocés dejanos un comentario sobre tu experiencia.
-            </h1>
-            <h1 className={`${styles.subtitle} ${abyssinica.className}`}>
-              Y si no, ¡Te invitamos a conocernos!
-            </h1>
-          </div>
+      <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          viewport={{ once: true }} // la animación se dispara solo una vez
+      >
+        <main className={styles.reviewsSection}>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <h1 className={`${styles.title} ${abyssinica.className}`}>
+                Si nos conocés dejanos un comentario sobre tu experiencia.
+              </h1>
+              <h1 className={`${styles.subtitle} ${abyssinica.className}`}>
+                Y si no, ¡Te invitamos a conocernos!
+              </h1>
+            </div>
 
-          <div className={styles.formSection}>
-            <form onSubmit={handleSubmit} className={styles.form}>
-              <div className={styles.inputGroup}>
-                <input
-                    type="text"
-                    placeholder="Acá tu nombre"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className={`${styles.input} ${styles.nameInput}`}
-                />
-                <span className={styles.inputLabel}>
-                Nombre y/o apellido (sino &quot;Anónimo&quot;)
-              </span>
-              </div>
+            <div className={styles.formSection}>
+              <form onSubmit={handleSubmit} className={styles.form}>
+                <div className={styles.inputGroup}>
+                  <input type="text" placeholder="Acá tu nombre" value={name}
+                         onChange={(e) => setName(e.target.value)}
+                         className={`${styles.input} ${styles.nameInput}`}/>
+                  <span className={styles.inputLabel}> Nombre y/o apellido (sino &quot;Anónimo&quot;) </span>
+                </div>
 
-              <div className={styles.inputGroup}>
-              <textarea
-                  placeholder="Acá tu experiencia"
-                  value={experience}
-                  onChange={(e) => {
-                    setExperience(e.target.value);
-                    const target = e.target as HTMLTextAreaElement;
-                    target.style.height = "auto";
-                    target.style.height = target.scrollHeight + "px";
-                  }}
-                  className={`${styles.input} ${styles.experienceInput}`}
-                  rows={2}
-                  required
-              />
-                <span className={styles.inputLabel}>
-                Contá acá tu experiencia con &quot;Agapanthus - Cabañas Las Marias&quot;
-              </span>
-              </div>
+                <div className={styles.inputGroup}>
+                <textarea placeholder="Acá tu experiencia" value={experience}
+                          onChange={(e) => {
+                            setExperience(e.target.value);
+                            const target = e.target as HTMLTextAreaElement;
+                            target.style.height = "auto";
+                            target.style.height = target.scrollHeight + "px";
+                          }} className={`${styles.input} ${styles.experienceInput}`}
+                          rows={2} required/>
+                  <span className={styles.inputLabel}>
+                  Contá acá tu experiencia con &quot;Agapanthus - Cabañas Las Marias&quot;
+                </span>
+                </div>
 
-              <button
-                  type="submit"
-                  className={`${styles.submitButton} ${abyssinica.className} ${buttonActive ? styles.activeButton : ""}`}
-              >
-                <h4>{buttonText}</h4>
-              </button>
-            </form>
-          </div>
-        </div>
+                <button type="submit"
+                        className={`${styles.submitButton} ${abyssinica.className} ${buttonActive ? styles.activeButton : ""}`}>
+                  <h4>{buttonText}</h4>
+                </button>
 
-        <div className={styles.reviewsCarousel}>
-          <div className={styles.scrollingWrapper}>
-            <div
-                className={styles.scrollingContent}
-                style={{ animationDuration: `${animationDuration}s` }}
-            >
-              {opiniones.map((review) => (
-                  <div key={review.id} className={styles.reviewCard}>
-                    <h4 className={`${styles.reviewName} ${abyssinica.className}`}>
-                      {review.autor}
-                    </h4>
-                    <p className={`${styles.reviewText} ${abyssinica.className}`}>
-                      {review.contenido}
-                    </p>
-                  </div>
-              ))}
+              </form>
             </div>
           </div>
-        </div>
-      </section>
+          <div className={styles.reviewsCarousel}>
+            <div className={styles.scrollingWrapper}>
+              <div className={styles.scrollingContent}> {opiniones.map((review) => (
+                  <div key={review.id} className={styles.reviewCard}><h4
+                      className={`${styles.reviewName} ${abyssinica.className}`}> {review.autor} </h4> <p
+                      className={`${styles.reviewText} ${abyssinica.className}`}> {review.contenido} </p>
+                  </div>))}
+              </div>
+            </div>
+          </div>
+        </main>
+      </motion.div>
   );
 }
