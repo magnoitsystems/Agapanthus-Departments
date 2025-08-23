effdc75c6d6b90d2a1a7d034b78c6de3122f7e8e
Ana Bellinzona <anabellinzona@gmail.com>
2025-08-23 17:36:15 -0300

    add the function for receive a email with the reserve information

--- src/components/successAlert/successAlert.tsx
+++ src/components/successAlert/successAlert.tsx
@@ -1,49 +1,58 @@
 import styles from './successAlert.module.css';
 import Image from "next/image";
+import {useState} from "react";
 
 type Props = {
     message1: string,
-    message2: string,
-    submessage: string,
-    onClose: () => void;
+    message2?: string,
+    submessage?: string
 }
 
-export default function SuccessAlert({message1, message2, submessage, onClose}: Props) {
+export default function SuccessAlert({message1, message2, submessage}: Props) {
+    const [show, setShow] = useState(false);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setShow(true);
+    };
+
     return (
-        <main className={styles.main}>
-            <div className={styles.cross} onClick={onClose} style={{cursor: "pointer"}}>
-                <Image
-                    src={'/icons/X.png'}
-                    alt={'cruz para cerrar cartel'}
-                    width={24}
-                    height={24}
-                />
-            </div>
-            <div className={styles.info}>
-                <div className={styles.title}>
-                    <h2>{message1}</h2>
-                    <h2>{message2}</h2>
+        !show && (
+            <main className={styles.main}>
+                <div className={styles.cross} onClick={handleSubmit} style={{cursor: "pointer"}}>
+                    <Image
+                        src={'/icons/X.png'}
+                        alt={'cruz para cerrar cartel'}
+                        width={24}
+                        height={24}
+                    />
                 </div>
-                <div className={styles.details}>
-                    <div className={styles.submessage}>
-                        <h4>{submessage}</h4>
+                <div className={styles.info}>
+                    <div className={styles.title}>
+                        <h2>{message1}</h2>
+                        <h2>{message2}</h2>
                     </div>
-                    <div className={styles.socialMedia}>
-                        <Image
-                            src={"/icons/instagram.png"}
-                            alt={"instagram logo"}
-                            width={44}
-                            height={44}
-                        />
-                        <Image
-                            src={"/icons/facebook.png"}
-                            alt={"facebook logo"}
-                            width={44}
-                            height={44}
-                        />
+                    <div className={styles.details}>
+                        <div className={styles.submessage}>
+                            <h4>{submessage}</h4>
+                        </div>
+                        <div className={styles.socialMedia}>
+                            <Image
+                                src={"/icons/instagram.png"}
+                                alt={"instagram logo"}
+                                width={44}
+                                height={44}
+                            />
+                            <Image
+                                src={"/icons/facebook.png"}
+                                alt={"facebook logo"}
+                                width={44}
+                                height={44}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
+            </main>
+        )
     );
 }
